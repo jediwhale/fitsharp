f44113b1b9380487b065d8640d391417bcfabfe9
jediwhale <mikes@.(none)>
2009-03-17 13:38:18 -0700

    clean up

--- source/fitSharp/Model/Tree.cs
+++
@@ -1,7 +0,0 @@
-namespace fitSharp.Model {
-    public interface TreeWriter<T> {
-        void WritePrefix(Tree<T> tree);
-        void WriteBranch(Tree<T> tree, int index);
-        void WriteSuffix(Tree<T> tree);
-    }
-}
