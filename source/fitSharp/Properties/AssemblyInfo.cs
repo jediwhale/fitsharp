19a0ee95f520441f1b72a873808fcdbf49fd7e9f
jediwhale <mikes@.(none)>
2009-03-26 21:26:01 -0700

    versions

--- source/fitSharp/Properties/AssemblyInfo.cs
+++ source/fitSharp/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Functional test tools for .NET")]
 [assembly: AssemblyDescription("fitSharp. The use and distribution terms for this software are covered by the Common Public License 1.0.")]
 [assembly: AssemblyProduct("fitSharp")]
-[assembly: AssemblyCopyright("Copyright © Syterra Software Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2009 Syterra Software Inc. All rights reserved.")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
--- source/fitTest/Properties/AssemblyInfo.cs
+++ source/fitTest/Properties/AssemblyInfo.cs
@@ -1,36 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("fitTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("fitTest")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Tests for Fit")]
+[assembly: AssemblyDescription("Fit. This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2.")]
+[assembly: AssemblyProduct("fitSharp")]
+[assembly: AssemblyCopyright("Copyright © 2009 Syterra Software Inc. All rights reserved.")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("011fcbdd-26c8-4c17-b0f3-0098aefa57a2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.*")]
