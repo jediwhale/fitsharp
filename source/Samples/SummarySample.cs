b42e138bf08a4f1a595a1f588f874f0318e28ac3
jediwhale <mikes@.(none)>
2009-12-19 18:36:38 -0700

    clean up tests

---
+++ source/Samples/SummarySample.cs
@@ -0,0 +1,10 @@
+﻿using fitlibrary;
+
+namespace fitSharp.Samples {
+    public class SummarySample: DoFixture {
+
+        public void AddLink() {
+            TestStatus.Summary["mylink"] = "<a href=\"somewhere\">somewhere</a>";
+        }
+    }
+}
