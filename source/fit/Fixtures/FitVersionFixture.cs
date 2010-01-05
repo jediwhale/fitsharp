52f719c08db1fb9a6a8667671fc7c3c6913eed3e
jediwhale <mikes@.(none)>
2010-01-05 14:27:20 -0700

    update docs

--- source/fit/Fixtures/FitVersionFixture.cs
+++ source/fit/Fixtures/FitVersionFixture.cs
@@ -3,9 +3,11 @@
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
+using System;
 using fitSharp.Machine.Application;
 
 namespace fit {
+    [Obsolete("Use suite configuration file")]
     public class FitVersionFixture : Fixture {
 
         public override bool IsVisible {
