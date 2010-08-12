68d30f7f109322c1f0384e6449b867e5f6a3d65b
Mike Stockdale <jediwhale@syterra.com>
2010-08-12 09:58:04 +0800

    add symbol info only once

--- source/fitSharp/Machine/Engine/Processor.cs
+++ source/fitSharp/Machine/Engine/Processor.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2009 Syterra Software Inc. All rights reserved.
+﻿// Copyright © 2010 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
@@ -121,7 +121,7 @@ namespace fitSharp.Machine.Engine {
             return result;
         }
 
-        public TypedValue Parse(Type type, TypedValue instance, Tree<T> parameters) {
+        public virtual TypedValue Parse(Type type, TypedValue instance, Tree<T> parameters) {
             TypedValue result = TypedValue.Void;
             Operators.Do<ParseOperator<T>>(
                 o => o.CanParse(type, instance, parameters),
