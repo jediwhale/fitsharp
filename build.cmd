7b9a832dbad1cdc0e85ec0aac54ed6f327124cb0
Kim Grasman <kim.grasman@gmail.com>
2010-11-16 12:45:40 +0100

    Revert "Using .NET 4.0 MSBuild. Better forwarding of arguments."
    This reverts commit 1a3c52d832087c50087836d7092af628e966cf6a.

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 @echo off
-"%WINDIR%\microsoft.net\framework\v4.0.30319\msbuild.exe" /nologo build.proj %*
+"%WINDIR%\microsoft.net\framework\v3.5\msbuild.exe" /nologo build.proj %1 %2 %3 %4 %5 %6 %7 %8 %9
