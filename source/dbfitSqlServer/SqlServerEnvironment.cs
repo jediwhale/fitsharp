cfcc38e41d29dfea8bc9b6efd19871f6755c21ec
Mike Stockdale <jediwhale@syterra.com>
2010-12-22 16:57:37 -0700

    support datetime2 type

--- source/dbfitSqlServer/SqlServerEnvironment.cs
+++ source/dbfitSqlServer/SqlServerEnvironment.cs
@@ -123,6 +123,7 @@ namespace dbfit
         private static string[] StringTypes = new string[] { "VARCHAR", "NVARCHAR", "CHAR", "NCHAR","TEXT","NTEXT","XML"};
         private static string[] DecimalTypes = new string[] { "DECIMAL", "NUMERIC", "MONEY", "SMALLMONEY" };
         private static string[] DateTypes = new string[] { "SMALLDATETIME","DATETIME","TIMESTAMP" };
+        private static string[] DateTypes2008 = new string[] { "DATETIME2" };
         private static string[] RefCursorTypes = new string[] { "REF" };
         private static string[] Int8Types = new string[] { "TINYINT" };
         private static string[] Int16Types = new string[] { "SMALLINT" };
@@ -153,6 +154,7 @@ namespace dbfit
             if (Array.IndexOf(StringTypes, dataType) >= 0) return SqlDbType.VarChar;
             if (Array.IndexOf(DecimalTypes, dataType) >= 0) return SqlDbType.Decimal;
             if (Array.IndexOf(DateTypes, dataType) >= 0) return SqlDbType.DateTime;
+            if (Array.IndexOf(DateTypes2008, dataType) >= 0) return SqlDbType.DateTime2;
             if (Array.IndexOf(Int8Types, dataType) >= 0) return SqlDbType.TinyInt;
             if (Array.IndexOf(Int16Types, dataType) >= 0) return SqlDbType.SmallInt;
             if (Array.IndexOf(Int32Types, dataType) >= 0) return SqlDbType.Int;
@@ -177,6 +179,7 @@ namespace dbfit
             if (Array.IndexOf(Int32Types, dataType) >= 0) return typeof(Int32);
             if (Array.IndexOf(Int64Types, dataType) >= 0) return typeof(Int64);
             if (Array.IndexOf(DateTypes, dataType) >= 0) return typeof(DateTime);
+            if (Array.IndexOf(DateTypes2008, dataType) >= 0) return typeof(DateTime);
             if (Array.IndexOf(RefCursorTypes, dataType) >= 0) return typeof(DataTable);
             if (Array.IndexOf(BooleanTypes, dataType) >= 0) return typeof(bool);
 			if (Array.IndexOf(BinaryTypes, dataType) >= 0) return typeof(byte[]);
