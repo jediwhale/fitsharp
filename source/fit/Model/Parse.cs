5b75cc1ad1d15a0c475ec409097f79ded27d1c5f
Mike Stockdale <jediwhale@syterra.com>
2011-03-19 16:09:06 -0600

    avoid deadlock on appdoamin unload

--- source/fit/Model/Parse.cs
+++ source/fit/Model/Parse.cs
@@ -1,4 +1,4 @@
-// Copyright © 2010 Syterra Software Inc. Includes work by Object Mentor, Inc., © 2002 Cunningham & Cunningham, Inc.
+// Copyright © 2011 Syterra Software Inc. Includes work by Object Mentor, Inc., © 2002 Cunningham & Cunningham, Inc.
 // This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2.
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
@@ -8,7 +8,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Web;
 using fitSharp.Fit.Operators;
 using fitSharp.Machine.Model;
 using fitSharp.Parser;
