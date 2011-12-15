01d6cd71f4d134e66192cd7ddf574982a35eb376
Mike Stockdale <jediwhale@syterra.com>
2011-12-15 16:33:08 -0700

    include fixture: parse and execute text

--- source/fitSharp/Machine/Model/MemberName.cs
+++ source/fitSharp/Machine/Model/MemberName.cs
@@ -3,12 +3,35 @@
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
 
+using System.Collections.Generic;
+
 namespace fitSharp.Machine.Model {
     public class MemberName {
         public const string NamedParameterPrefix = "!";
 
+        public static MemberName Parse(string name) {
+            if (name.EndsWith("?")) name = name.Substring(0, name.Length - 1);
+            if (name.Length > 0 && char.IsDigit(name, 0)) {
+                name = digitConversion[name[0]] + name.Substring(1);
+            }
+            return new MemberName(new GracefulName(name).IdentifierName.ToString());
+        }
+
         public MemberName(string name) { this.name = name; }
         public override string ToString() { return name; }
+
+        static readonly Dictionary<char, string> digitConversion = new Dictionary<char, string> {
+                {'0', "zero"},
+                {'1', "one"},
+                {'2', "two"},
+                {'3', "three"},
+                {'4', "four"},
+                {'5', "five"},
+                {'6', "six"},
+                {'7', "seven"},
+                {'8', "eight"},
+                {'9', "nine"}
+            };
 
         readonly string name;
     }
---
+++ source/fitSharpTest/NUnit/Fit/IncludeTest.cs
@@ -0,0 +1,66 @@
+﻿// Copyright © 2011 Syterra Software Inc. All rights reserved.
+// The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
+// to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
+
+using System;
+using fitSharp.Fit.Fixtures;
+using fitSharp.Fit.Model;
+using fitSharp.Fit.Operators;
+using fitSharp.Fit.Service;
+using fitSharp.Machine.Engine;
+using fitSharp.Machine.Model;
+using fitSharp.Test.Double.Fit;
+using NUnit.Framework;
+
+namespace fitSharp.Test.NUnit.Fit {
+    [TestFixture] public class IncludeTest {
+        [Test] public void ParsesAndExecutesIncludedText() {
+            var processor = Builder.CellProcessor();
+            processor.AddOperator(new MockRunTestOperator());
+            processor.AddOperator(new MockComposeStoryTestString());
+            processor.AddOperator(new MockParseStoryTestString());
+            var includeTable = new CellTree(new CellTree("include", "text", input));
+            new Include().Interpret(processor, includeTable);
+            Assert.IsTrue(includeTable.Branches[0].Branches[0].Value.HasAttribute(CellAttribute.Folded));
+            Assert.AreEqual(result, includeTable.Branches[0].Branches[0].Value.GetAttribute(CellAttribute.Folded));
+        }
+
+        const string input = "stuff";
+        const string result = "more stuff";
+        static readonly Tree<Cell> parsedInput = new CellTree("something");
+
+        class MockRunTestOperator: CellOperator, RunTestOperator {
+            public bool CanRunTest(Tree<Cell> testTables, StoryTestWriter writer) {
+                return true;
+            }
+
+            public TypedValue RunTest(Tree<Cell> testTables, StoryTestWriter writer) {
+                Assert.AreSame(parsedInput, testTables);
+                writer.WriteTable(parsedInput);
+                return TypedValue.Void;
+            }
+        }
+
+        class MockComposeStoryTestString: CellOperator, ComposeOperator<Cell> {
+            public bool CanCompose(TypedValue instance) {
+                return instance.Type == typeof(StoryTestString);
+            }
+
+            public Tree<Cell> Compose(TypedValue instance) {
+                Assert.AreEqual(input, instance.ValueString);
+                return parsedInput;
+            }
+        }
+
+        class MockParseStoryTestString: CellOperator, ParseOperator<Cell> {
+            public bool CanParse(Type type, TypedValue instance, Tree<Cell> parameters) {
+                return type == typeof(StoryTableString);
+            }
+
+            public TypedValue Parse(Type type, TypedValue instance, Tree<Cell> parameters) {
+                return new TypedValue(new StoryTableString(result));
+            }
+        }
+    }
+}
