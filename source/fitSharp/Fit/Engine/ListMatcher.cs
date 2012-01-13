582b03906e5f12e4eb5a2933326472968d858c6a
Mike Stockdale <jediwhale@syterra.com>
2012-01-12 19:58:59 -0700

    improve list matching algorithm

--- source/fitSharp/Fit/Engine/ListMatcher.cs
+++ source/fitSharp/Fit/Engine/ListMatcher.cs
@@ -21,9 +21,9 @@ namespace fitSharp.Fit.Engine {
 
         public bool IsEqual(IEnumerable<object> theActualValue, Tree<Cell> theExpectedValueCell) {
             var actuals = new Actuals(theActualValue, strategy);
-            int expectedRow = 0;
+            var expectedRow = 0;
             foreach (var currentRow in theExpectedValueCell.Branches[0].Branches.Skip(1)) {
-                int match = actuals.FindMatch(RowMatches, expectedRow, currentRow);
+                int match = actuals.FindMatch(ExactMatch, expectedRow, currentRow);
                 if (match < 0 || (match != expectedRow && strategy.IsOrdered)) return false;
                 expectedRow++;
             }
@@ -35,11 +35,11 @@ namespace fitSharp.Fit.Engine {
             if (table.Branches.Count == rowsToSkip + 1 && actuals.UnmatchedCount == 0) {
                 processor.TestStatus.MarkRight(table.ValueAt(rowsToSkip));
             }
-            bool result = true;
-            int expectedRow = 0;
+            var result = true;
+            var expectedRow = 0;
             foreach (var currentRow in table.Branches.Skip(rowsToSkip + 1)) {
                 try {
-                    int match = actuals.FindMatch(RowMatches, expectedRow, currentRow);
+                    var match = actuals.FindMatch(RowMatches, expectedRow, currentRow);
                     if (match < 0) {
                         MarkAsIncorrect(currentRow, "missing");
                         result = false;
@@ -56,19 +56,18 @@ namespace fitSharp.Fit.Engine {
                 result = false;
             }
 
-            for (int row = 0; row < expectedRow; row++) {
+            for (var row = 0; row < expectedRow; row++) {
                 var markRow = table.Branches[rowsToSkip + row + 1];
                 if (strategy.IsOrdered && actuals.IsOutOfOrder(row)) {
                     MarkAsIncorrect(markRow, "out of order");
                     result = false;
                 }
                 else if (actuals.Match(row) != null) {
-                    TypedValue[] actualValues = strategy.ActualValues(actuals.Match(row));
+                    var actualValues = strategy.ActualValues(actuals.Match(row));
                     int i = 0;
                     foreach (var cell in markRow.Branches) {
                         if (actualValues[i].Type != typeof(void) || cell.Value.Text.Length > 0) {
                              processor.Check(actualValues[i], cell);
-
                         }
                         i++;
                     }
@@ -85,23 +84,37 @@ namespace fitSharp.Fit.Engine {
             processor.TestStatus.MarkWrong(theRow.Value);
         }
 
-        bool RowMatches(Tree<Cell> expectedCells, object theActualRow) {
-            if (!strategy.IsExpectedSize(expectedCells.Branches.Count, theActualRow)) return false;
-            int i = 0;
-            foreach (TypedValue actualValue in strategy.ActualValues(theActualRow)) {
-                if (actualValue.Type != typeof(void) || expectedCells.ValueAt(i).Text.Length > 0) {
-                    if (!strategy.CellMatches(actualValue, expectedCells.Branches[i], i)) return false;
+        int ExactMatch(Tree<Cell> expectedCells, object actualRow) {
+            return RowMatches(expectedCells, actualRow) == expectedCells.Branches.Count ? 1 : 0;
+        }
+
+        int RowMatches(Tree<Cell> expectedCells, object theActualRow) {
+            if (!strategy.IsExpectedSize(expectedCells.Branches.Count, theActualRow)) return 0;
+            var i = 0;
+            var matches = 0;
+            var voids = 0;
+            var mismatches = 0;
+            foreach (var actualValue in strategy.ActualValues(theActualRow)) {
+                if (actualValue.Type == typeof(void) && expectedCells.ValueAt(i).Text.Length == 0) {
+                    voids++;
+                }
+                else if (strategy.CellMatches(actualValue, expectedCells.Branches[i], i)) {
+                    matches++;
+                }
+                else {
+                    mismatches++;
                 }
                 i++;
             }
-            return true;
+            return matches == 0 && mismatches > 0 ? 0 : matches + voids;
         }
 
         class Actuals {
             readonly List<ActualItem> myActuals;
             readonly ListMatchStrategy myStrategy;
 
-            public delegate bool Matches(Tree<Cell> theExpectedCells, object theActual);
+            public delegate int Matches(Tree<Cell> theExpectedCells, object theActual);
+
             public Actuals(IEnumerable<object> theActualValues, ListMatchStrategy theStrategy) {
                 myActuals = new List<ActualItem>(theActualValues.Select(o => new ActualItem(o)));
                 myStrategy = theStrategy;
@@ -115,31 +128,42 @@ namespace fitSharp.Fit.Engine {
             }
 
             public int FindMatch(Matches theMatcher, int theExpectedRow, Tree<Cell> theExpectedCells) {
-                int result = -1;
-                if (myActuals.Count == 0) return result;
-                int lastMatched = -1;
-                for (int row = 0; row < myActuals.Count; row++) {
-                    if (myActuals[row].MatchRow != null) {
-                        lastMatched = row;
-                        continue;
-                    }
-                    if (result == -1 && theMatcher(theExpectedCells, myActuals[row].Value)) {
-                        result = row;
-                    }
+                if (myActuals.Count == 0) return -1;
+
+                var bestMatchScore = 0;
+                var bestMatchRow = -1;
+
+                for (var row = 0; row < myActuals.Count; row++) {
+                    if (myActuals[row].MatchRow.HasValue) continue;
+                    var matchScore = theMatcher(theExpectedCells, myActuals[row].Value);
+                    if (matchScore <= bestMatchScore) continue;
+
+                    bestMatchScore = matchScore;
+                    bestMatchRow = row;
                 }
-                if (result > -1) {
-                    myActuals[result].MatchRow = theExpectedRow;
-                    UnmatchedCount--;
-                } else {
-                    myActuals.Insert(lastMatched + 1, new ActualItem(null, -1));
+
+                if (bestMatchScore <= 0) {
+                    InsertPlaceholderForMissingRow();
+                    return -1;
                 }
-                return result;
+
+                myActuals[bestMatchRow].MatchRow = theExpectedRow;
+                UnmatchedCount--;
+                return bestMatchRow;
+            }
+
+            void InsertPlaceholderForMissingRow() {
+                var lastMatched = -1;
+                for (var row = 0; row < myActuals.Count; row++) {
+                    if (myActuals[row].MatchRow != null) lastMatched = row;
+                }
+                myActuals.Insert(lastMatched + 1, ActualItem.Missing);
             }
 
             public void ShowSurplus(CellProcessor processor, Tree<Cell> table) {
-                for (int i = 0; i < myActuals.Count;) {
-                    ActualItem surplus = myActuals[i];
-                    if (surplus.MatchRow != null) {
+                for (var i = 0; i < myActuals.Count;) {
+                    var surplus = myActuals[i];
+                    if (surplus.MatchRow.HasValue) {
                         i++;
                         continue;
                     }
@@ -157,7 +181,7 @@ namespace fitSharp.Fit.Engine {
 
             Tree<Cell> MakeSurplusRow(CellProcessor processor, object theSurplusRow) {
                 var cells = new List<Tree<Cell>>();
-                foreach (TypedValue actualValue in myStrategy.ActualValues(theSurplusRow)) {
+                foreach (var actualValue in myStrategy.ActualValues(theSurplusRow)) {
                     var cell = processor.Compose(actualValue.IsVoid ? new TypedValue(string.Empty) : actualValue);
                     if (cells.Count == 0) {
                         cell.Value.SetAttribute(CellAttribute.Label, "surplus");
@@ -170,13 +194,17 @@ namespace fitSharp.Fit.Engine {
             }
 
             class ActualItem {
+                public static readonly ActualItem Missing = new ActualItem(null, -1);
+
                 public readonly object Value;
                 public int? MatchRow;
+
                 public ActualItem(object theValue) {
                     Value = theValue;
                     MatchRow = null;
                 }
-                public ActualItem(object theValue, int theMatchRow) {
+
+                ActualItem(object theValue, int theMatchRow) {
                     Value = theValue;
                     MatchRow = theMatchRow;
                 }
