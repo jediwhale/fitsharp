8d6ae86444a17b12616761527b179f0e0da28c14
Mike Stockdale <jediwhale@syterra.com>
2012-02-27 20:22:39 -0700

    release 2.1

--- source/Runner/Properties/AssemblyInfo.cs
+++ source/Runner/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Runner for fitSharp")]
 [assembly: AssemblyDescription("fitSharp. The use and distribution terms for this software are covered by the Common Public License 1.0.")]
 [assembly: AssemblyProduct("fitSharp")]
-[assembly: AssemblyCopyright("Copyright © 2011 Syterra Software Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2012 Syterra Software Inc. All rights reserved.")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyVersion("2.1.*")]
--- source/fitTest/Properties/AssemblyInfo.cs
+++ source/fitTest/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Tests for Fit")]
 [assembly: AssemblyDescription("Fit. This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2.")]
 [assembly: AssemblyProduct("fitSharp")]
-[assembly: AssemblyCopyright("Copyright © 2011 Syterra Software Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2012 Syterra Software Inc. All rights reserved.")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyVersion("2.1.*")]
