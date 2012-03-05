83d5d967bd8f0c339a50f921cef391e1184d8ec2
Mike Stockdale <jediwhale@syterra.com>
2012-03-05 15:58:58 -0700

    comfigure processor and with multiple rows

--- source/fitSharp/Fit/Fixtures/ConfigureFixture.cs
+++ source/fitSharp/Fit/Fixtures/ConfigureFixture.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2011 Syterra Software Inc. All rights reserved.
+﻿// Copyright © 2012 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
@@ -12,14 +12,29 @@ namespace fitSharp.Fit.Fixtures {
         public void Interpret(CellProcessor processor, Tree<Cell> table) {
             processor.TestStatus.TableCount--;
 
-            var facility = processor.Memory.GetItem(table.ValueAt(0, 1).Text);
-            var currentRow = table.Branches[0].Skip(2);
+            var facility = processorIdentifier.Matches(table.ValueAt(0, 1).Text)
+                ? processor
+                : processor.Memory.GetItem(table.ValueAt(0, 1).Text);
+
+            if (table.Branches[0].Branches.Count > 2) {
+                var currentRow = table.Branches[0].Skip(2);
+                Execute(processor, facility, currentRow);
+            }
+
+            new Traverse<Cell>()
+                .Rows.All(row => Execute(processor, facility, row))
+                .VisitTable(table);
+        }
+
+        static void Execute(CellProcessor processor, object facility, Tree<Cell> currentRow) {
             var selector = new DoRowSelector();
             var result = processor.ExecuteWithThrow(facility, selector.SelectMethodCells(currentRow),
-                                                                    selector.SelectParameterCells(currentRow),
-                                                                    currentRow.ValueAt(0));
+                                                    selector.SelectParameterCells(currentRow),
+                                                    currentRow.ValueAt(0));
             if (result.IsVoid) return;
-            table.ValueAt(0, 2).SetAttribute(CellAttribute.Folded, result.ValueString);
+            currentRow.ValueAt(0).SetAttribute(CellAttribute.Folded, result.ValueString);
         }
+
+        readonly IdentifierName processorIdentifier = new IdentifierName("processor");
     }
 }
--- source/fitSharp/Machine/Model/Traverse.cs
+++ source/fitSharp/Machine/Model/Traverse.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2011 Syterra Software Inc. All rights reserved.
+﻿// Copyright © 2012 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
@@ -9,6 +9,11 @@ using System.Linq;
 namespace fitSharp.Machine.Model {
     public class Traverse<T> {
         public Traverse<T> Rows { get { return this; } }
+
+        public Traverse<T> All(Action<Tree<T>> visit) {
+            all = visit;
+            return this;
+        }
 
         public Traverse<T> Rest(Action<Tree<T>> visit) {
             rest = visit;
@@ -23,6 +28,7 @@ namespace fitSharp.Machine.Model {
         public void VisitTable(Tree<T> table) {
             var action = header;
             foreach (var row in table.Branches.Skip(1)) {
+                all(row);
                 action(row);
                 action = rest;
             }
@@ -30,5 +36,6 @@ namespace fitSharp.Machine.Model {
 
         Action<Tree<T>> header = t => {};
         Action<Tree<T>> rest = t => {};
+        Action<Tree<T>> all = t => {};
     }
 }
