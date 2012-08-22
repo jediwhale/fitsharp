b987e96012562033132bb879691137f55d4bf4d0
Mike Stockdale <jediwhale@syterra.com>
2012-08-21 18:36:58 -0600

    fix manual merge mess up

--- source/fitSharp/Fit/Operators/ParseNullArray.cs
+++ source/fitSharp/Fit/Operators/ParseNullArray.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2010 Syterra Software Inc. All rights reserved.
+﻿// Copyright © 2012 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
@@ -12,7 +12,7 @@ namespace fitSharp.Fit.Operators {
         private static readonly IdentifierName nullIdentifier = new IdentifierName("null");
 
         public bool CanParse(Type type, TypedValue instance, Tree<Cell> parameters) {
-            return type.IsArray && parameters.Value != null && nullIdentifier.Equals(parameters.Value.Text);
+            return type.IsArray && parameters.Value != null && nullIdentifier.Equals(parameters.Value.Content);
         }
 
         public TypedValue Parse(Type type, TypedValue instance, Tree<Cell> parameters) {
