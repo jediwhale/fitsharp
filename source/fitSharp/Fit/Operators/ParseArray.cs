8cf56a1905bfa181948fc35c005ce66904392489
Mike Stockdale <jediwhale@syterra.com>
2012-08-24 17:13:12 -0600

    parse empty array with blank keyword

--- source/fitSharp/Fit/Operators/ParseArray.cs
+++ source/fitSharp/Fit/Operators/ParseArray.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2009 Syterra Software Inc. All rights reserved.
+﻿// Copyright © 2012 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
@@ -18,7 +18,10 @@ namespace fitSharp.Fit.Operators {
         }
 
         public TypedValue Parse(Type type, TypedValue instance, Tree<Cell> parameters) {
-			string[] strings = parameters.Value.Text.Split(new [] {','});
+            var content = Processor.Parse<Cell, string>(parameters.Value);
+            var strings = !string.IsNullOrEmpty(content)
+                ? content.Split(new[] { ',' })
+                : new string[]{};
 
             if (type.IsArray) {
 			    Array array = Array.CreateInstance(type.GetElementType(), strings.Length);
