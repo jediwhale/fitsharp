28cd63a49225edb8a24c9ed7085508b5bf3fc1d9
Mike Stockdale <jediwhale@syterra.com>
2013-02-20 16:43:27 -0700

    clean: remove special action name hack

--- source/fitSharp/Fit/Operators/InvokeSpecialAction.cs
+++ source/fitSharp/Fit/Operators/InvokeSpecialAction.cs
@@ -1,22 +1,22 @@
-// Copyright © 2012 Syterra Software Inc. All rights reserved.
+// Copyright © 2013 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
 
 using System;
+using fitSharp.Fit.Engine;
 using fitSharp.Machine.Engine;
 using fitSharp.Machine.Exception;
 using fitSharp.Machine.Model;
 
 namespace fitSharp.Fit.Operators {
-    public class InvokeSpecialAction: CellOperator, InvokeOperator<Cell> {
+    public class InvokeSpecialAction: CellOperator, InvokeSpecialOperator {
 
-
-        public bool CanInvoke(TypedValue instance, MemberName memberName, Tree<Cell> parameters) {
-            return memberName.IsSpecialAction;
+        public bool CanInvokeSpecial(TypedValue instance, MemberName memberName, Tree<Cell> parameters) {
+            return true;
         }
 
-        public TypedValue Invoke(TypedValue instance, MemberName memberName, Tree<Cell> parameters) {
+        public TypedValue InvokeSpecial(TypedValue instance, MemberName memberName, Tree<Cell> parameters) {
             var type = Processor.ApplicationUnderTest.FindType("fit.Parse").Type;
 
     		// lookup Fixture
