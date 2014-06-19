7c057f05b12fb2bea4302a388fcb8e1bb270048f
Mike Stockdale <jediwhale@syterra.com>
2014-06-19 14:06:30 -0600

    release 2.3

--- source/TestTarget/Properties/AssemblyInfo.cs
+++ source/TestTarget/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Test target for fitSharp")]
 [assembly: AssemblyDescription("fitSharp. The use and distribution terms for this software are covered by the Common Public License 1.0.")]
 [assembly: AssemblyProduct("fitSharp")]
-[assembly: AssemblyCopyright("Copyright © 2012 Syterra Software Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2014 Syterra Software Inc. All rights reserved.")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.2.*")]
+[assembly: AssemblyVersion("2.3.*")]
--- source/dbfitMySql/Properties/AssemblyInfo.cs
+++ source/dbfitMySql/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DbFit MySql for fitSharp")]
 [assembly: AssemblyDescription("DbFit. This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2.")]
 [assembly: AssemblyProduct("fitSharp")]
-[assembly: AssemblyCopyright("Copyright © 2012 Syterra Software Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2014 Syterra Software Inc. All rights reserved.")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.2.*")]
+[assembly: AssemblyVersion("2.3.*")]
