fcf853f5c16a7ea95f890157768535087124776e
Mike Stockdale <mikes@syterra.com>
2015-10-26 15:44:29 -0600

    allow array of symbols

--- source/fitSharp/Fit/Operators/ParseSymbol.cs
+++ source/fitSharp/Fit/Operators/ParseSymbol.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2012 Syterra Software Inc. All rights reserved.
+﻿// Copyright © 2015 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
@@ -11,7 +11,9 @@ using fitSharp.Machine.Model;
 namespace fitSharp.Fit.Operators {
     public class ParseSymbol: CellOperator, ParseOperator<Cell> {
         public bool CanParse(Type type, TypedValue instance, Tree<Cell> parameters) {
-            return parameters.Value != null && parameters.Value.Content.StartsWith("<<");
+            return parameters.Value != null
+                && parameters.Value.Content.StartsWith("<<")
+                && !parameters.Value.Content.Contains(",");
         }
 
         public TypedValue Parse(Type type, TypedValue instance, Tree<Cell> parameters) {
@@ -25,7 +27,7 @@ namespace fitSharp.Fit.Operators {
         }
 
         TypedValue MakeTypedValue(object value, Type type) {
-            return value == null || type.IsAssignableFrom(value.GetType())
+            return value == null || type.IsInstanceOfType(value)
                 ? new TypedValue(value, type)
                 : Processor.ParseString(type, value.ToString());
         }
--- source/fitSharpTest/NUnit/Fit/ParseSymbolTest.cs
+++ source/fitSharpTest/NUnit/Fit/ParseSymbolTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2012 Syterra Software Inc. All rights reserved.
+﻿// Copyright © 2015 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
@@ -17,6 +17,7 @@ namespace fitSharp.Test.NUnit.Fit {
             Assert.IsTrue(CanParse<string>("\t<<symbol\r\n"), "\t<<symbol\r\n");
             Assert.IsFalse(CanParse<string>("<nosymbol"), "<nosymbol");
             Assert.IsFalse(CanParse<string>("nosymbol<<"), "nosymbol<<");
+            Assert.IsFalse(CanParse<string>("<<symbol1,other"), "array");
         }
 
         [Test] public void Parse() {
