b0ea1a5bf63a0783ddd1694a40053e3ae34e5c25
Sebastian Mitterle <sebastian.mitterle@mediatechsolutions.es>
2015-10-30 10:37:37 +0100

    Added new fixture for checking sub lists unordered

---
+++ source/fit/Fixtures/ArrayUnorderedSubFixture.cs
@@ -0,0 +1,62 @@
+﻿namespace fit.Fixtures
+{
+    using fit.Operators;
+    using fitlibrary;
+    using fitSharp.Fit.Engine;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// This fixture compares the items of a list or array to items in the wiki.
+    /// It works like the ArrayFixture but won't check order and surplus items will be ignored.
+    /// </summary>
+    public class ArrayUnorderedSubFixture : NamedCollectionFixtureBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArraySubFixture"/> class.
+        /// </summary>
+        public ArrayUnorderedSubFixture(): base() {}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArraySubFixture"/> class.
+        /// </summary>
+        /// <param name="theArray">The array.</param>
+        public ArrayUnorderedSubFixture(IEnumerable<object> theArray): base(theArray) {}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArraySubFixture"/> class.
+        /// </summary>
+        /// <param name="theCollection">The collection.</param>
+        public ArrayUnorderedSubFixture(IEnumerable theCollection): base(theCollection) {}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArraySubFixture"/> class.
+        /// </summary>
+        /// <param name="theEnumerator">The enumerator.</param>
+        public ArrayUnorderedSubFixture(IEnumerator theEnumerator): base(theEnumerator) {}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArraySubFixture"/> class.
+        /// </summary>
+        /// <param name="theTable">The table.</param>
+        public ArrayUnorderedSubFixture(DataTable theTable) : base(theTable.Rows.GetEnumerator()) { }
+
+        /// <summary>
+        /// Gets the match strategy.
+        /// </summary>
+        /// <value>
+        /// The match strategy.
+        /// </value>
+        protected override ListMatchStrategy MatchStrategy
+        {
+            get 
+            {
+                return new ArrayUnorderedSubMatchStrategy(this.Processor, this.myHeaderRow);
+            }
+        }
+    }
+}
---
+++ source/fit/Operators/ArrayUnorderedSubMatchStrategy.cs
@@ -0,0 +1,39 @@
+﻿namespace fit.Operators
+{
+    using fitSharp.Fit.Engine;
+    using fitSharp.Machine.Model;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// This strategy is used in the ArraySubFixture in order to only compare items that are in the list.
+    /// Not the whole list of actual items must exist nor must they be ordered.
+    /// </summary>
+    public class ArrayUnorderedSubMatchStrategy : NamedMatchStrategy
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArraySubMatchStrategy"/> class.
+        /// </summary>
+        /// <param name="processor">The processor.</param>
+        /// <param name="theHeaderRow">The header row.</param>
+        public ArrayUnorderedSubMatchStrategy(CellProcessor processor, Tree<Cell> theHeaderRow) : base(processor, theHeaderRow) { }
+        
+        /// <summary>
+        /// Gets a value indicating whether items in compared lists have to be ordered.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if compared lists have to have the same order; otherwise, <c>false</c>.
+        /// </value>
+        public override bool IsOrdered {get { return false; }}
+        
+        /// <summary>
+        /// Gets a value indicating whether additional elements in the actual list are marked as failure.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if additional elements in the actual list are allowed; otherwise, <c>false</c>.
+        /// </value>
+        public override bool SurplusAllowed {get {return true;}}
+    }
+}
