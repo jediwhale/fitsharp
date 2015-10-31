ce9e0853a28b4e038e963dd20e65a3a705746e77
Mike Stockdale <mikes@syterra.com>
2015-10-31 15:19:45 -0600

    fix namespace

--- source/fitSharp/Machine/Application/AssemblyLoadFailureHandler.cs
+++ source/fitSharp/Machine/Application/AssemblyLoadFailureHandler.cs
@@ -1,11 +1,15 @@
-﻿using System;
-using System.Collections;
+﻿// Copyright © 2015 Syterra Software Inc. All rights reserved.
+// The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
+// which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
+// to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 
-namespace fitSharp.Slim.Service
+namespace fitSharp.Machine.Application
 {
     /// <summary>
     /// Handles cases where an Assembly.LoadFrom() fails, potentially due to attempting to load external dependencies.
--- source/fitSharp/Machine/Application/Shell.cs
+++ source/fitSharp/Machine/Application/Shell.cs
@@ -1,4 +1,4 @@
-// Copyright © 2011 Syterra Software Inc. All rights reserved.
+// Copyright © 2015 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
@@ -9,7 +9,6 @@ using System.Reflection;
 using System.Threading;
 using fitSharp.IO;
 using fitSharp.Machine.Engine;
-using fitSharp.Slim.Service;
 
 namespace fitSharp.Machine.Application {
 
