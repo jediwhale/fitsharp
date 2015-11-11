26c1e0ba126ae91bce5883ce446f800f4c5486eb
Mike Stockdale <mikes@syterra.com>
2015-11-11 14:09:48 -0700

    refactor namespace dependency

--- source/fitSharp/Fit/Engine/InterpretFlow.cs
+++ source/fitSharp/Fit/Engine/InterpretFlow.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2013 Syterra Software Inc. All rights reserved.
+﻿// Copyright © 2015 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
@@ -6,12 +6,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using fitSharp.Fit.Engine;
 using fitSharp.Machine.Engine;
 using fitSharp.Machine.Exception;
 using fitSharp.Machine.Model;
 
-namespace fitSharp.Fit.Operators {
+namespace fitSharp.Fit.Engine {
     public class InterpretFlow: InterpretTableFlow {
         public InterpretFlow(CellProcessor processor, FlowInterpreter interpreter) {
             this.interpreter = interpreter;
