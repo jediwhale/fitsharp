267f163a1d8801c121fd58f6530df8514c5e8eea
Mike Stockdale <mikes@syterra.com>
2016-11-17 20:51:25 -0700

    release 2.5

--- source/Runner/Properties/AssemblyInfo.cs
+++ source/Runner/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Runner for fitSharp")]
 [assembly: AssemblyDescription("fitSharp. The use and distribution terms for this software are covered by the Common Public License 1.0.")]
 [assembly: AssemblyProduct("fitSharp")]
-[assembly: AssemblyCopyright("Copyright © 2015 Syterra Software Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2016 Syterra Software Inc. All rights reserved.")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.4.*")]
+[assembly: AssemblyVersion("2.5.*")]
--- source/dbfitMySql/Properties/AssemblyInfo.cs
+++ source/dbfitMySql/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DbFit MySql for fitSharp")]
 [assembly: AssemblyDescription("DbFit. This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2.")]
 [assembly: AssemblyProduct("fitSharp")]
-[assembly: AssemblyCopyright("Copyright © 2015 Syterra Software Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2016 Syterra Software Inc. All rights reserved.")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.4.*")]
+[assembly: AssemblyVersion("2.5.*")]
