ac94b6db730b86cb84a03a6d69be1ca157d1d2f1
Mike Stockdale <mikes@syterra.com>
2017-11-04 15:22:02 -0600

    fitif ideas

---
+++ source/fitIf/Program.fs
@@ -0,0 +1,19 @@
+﻿open System.Net
+open System.Net.Sockets
+open System.Text
+
+[<EntryPoint>]
+let main argv = 
+    let listener = new TcpListener(IPAddress.Parse("127.0.0.1") ,8081)
+    listener.Start()
+    printfn "listening ..."
+    let client = listener.AcceptTcpClient()
+    printfn "accepted ..."
+    let stream = client.GetStream()
+    let bytes = Array.create<byte> 256 0uy
+    let length = stream.Read(bytes, 0, 256)
+    let line = Encoding.ASCII.GetString(bytes, 0, length)
+    printfn "%s" line
+    client.Close()
+    listener.Stop()
+    0
