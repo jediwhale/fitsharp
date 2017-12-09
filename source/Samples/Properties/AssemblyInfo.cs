83812f6573172eff25e26c68044dc46469a44288
Mike Stockdale <mikes@syterra.com>
2017-12-08 17:02:06 -0700

    release 2.6

--- source/Samples/Properties/AssemblyInfo.cs
+++ source/Samples/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Samples for fitSharp")]
 [assembly: AssemblyDescription("fitSharp. The use and distribution terms for this software are covered by the Common Public License 1.0.")]
 [assembly: AssemblyProduct("fitSharp")]
-[assembly: AssemblyCopyright("Copyright © 2016 Syterra Software Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2017 Syterra Software Inc. All rights reserved.")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.5.*")]
+[assembly: AssemblyVersion("2.6.*")]
