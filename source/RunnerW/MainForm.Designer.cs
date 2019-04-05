6fbcbfa823ebcfa00734829b3f2ed97fbde7e3f6
Vince Sipocz <Vince.Sipocz@pressganey.com>
2019-04-05 08:34:24 -0400

    Make the Go button the form's default Accept button

--- source/RunnerW/MainForm.Designer.cs
+++ source/RunnerW/MainForm.Designer.cs
@@ -75,6 +75,7 @@
             // 
             // MainForm
             // 
+            this.AcceptButton = this.goButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(632, 453);
