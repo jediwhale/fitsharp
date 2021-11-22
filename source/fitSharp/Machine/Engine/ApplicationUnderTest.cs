2ea6d84129182f1c0c75ad139ca7843a7eddc7ff
Mike Stockdale <mikes@syterra.com>
2021-11-22 16:18:49 -0700

    ignore 'defaultPath' from FitNesse

--- source/fitSharp/Machine/Engine/ApplicationUnderTest.cs
+++ source/fitSharp/Machine/Engine/ApplicationUnderTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2020 Syterra Software Inc. All rights reserved.
+﻿// Copyright © 2021 Syterra Software Inc. All rights reserved.
 // The use and distribution terms for this software are covered by the Common Public License 1.0 (http://opensource.org/licenses/cpl.php)
 // which can be found in the file license.txt at the root of this distribution. By using this software in any fashion, you are agreeing
 // to be bound by the terms of this license. You must not remove this notice, or any other, from this software.
@@ -140,7 +140,9 @@ namespace fitSharp.Machine.Engine {
             }
 
             static bool IsIgnored(string assemblyName) {
-                return string.Equals(".jar", Path.GetExtension(assemblyName), StringComparison.OrdinalIgnoreCase);
+                return
+                    string.Equals("defaultPath", assemblyName, StringComparison.Ordinal) ||
+                    string.Equals(".jar", Path.GetExtension(assemblyName), StringComparison.OrdinalIgnoreCase);
             }
 
             public IEnumerable<Type> Types {
