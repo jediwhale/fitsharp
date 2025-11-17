a89b5cf7a2f6a2642e993bee7539003609663754
Anton Herzog <info@antonherzog.com>
2025-11-17 10:22:21 +0100

    Copyright update

--- source/fit/Operators/FixtureTable.cs
+++ source/fit/Operators/FixtureTable.cs
@@ -1,4 +1,4 @@
-// Copyright © 2022 Syterra Software Inc.
+// Copyright Â© 2025 Syterra Software Inc.
 // This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2.
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
--- source/fitTest/NUnit/RowFixtureTest.cs
+++ source/fitTest/NUnit/RowFixtureTest.cs
@@ -1,4 +1,4 @@
-// Copyright � 2018 Syterra Software Inc. Includes work by Object Mentor, Inc., � 2002 Cunningham & Cunningham, Inc.
+// Copyright © 2025 Syterra Software Inc. Includes work by Object Mentor, Inc., © 2002 Cunningham & Cunningham, Inc.
 // This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License version 2.
 // This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
